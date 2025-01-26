7bbcc9bf038a1dd22a98319b62e71128ae299149
SantiCZ <141504608+SantiCZ@users.noreply.github.com>
2025-01-26 11:04:52 -0300

    Update guardar datos.cpp

--- guardar datos/guardar datos.cpp
+++ guardar datos/guardar datos.cpp
@@ -3,20 +3,20 @@
 #include <string>
 
 using namespace std;
-// Estructura para almacenar la información de una persona
+// Estructura para almacenar la informacion de una persona
 struct Persona {
     string nombre;
     string telefono;
 };
-// Función para guardar datos de personas en un archivo
+// Funcion para guardar datos de personas en un archivo
 void guardarPersonas(const string& nombreArchivo) {
-    const int numPersonas = 5;  // Número de personas a registrar
+    const int numPersonas = 5;  // Numero de personas a registrar
     Persona personas[numPersonas];
     // Obtener datos de las personas del usuario
     for (int i = 0; i < numPersonas; ++i) {
         cout << "Ingrese el nombre de la persona " << i + 1 << ": ";
         getline(cin, personas[i].nombre);
-        cout << "Ingrese el teléfono de la persona " << i + 1 << ": ";
+        cout << "Ingrese el telÃ©fono de la persona " << i + 1 << ": ";
         getline(cin, personas[i].telefono);
     }
     // Abrir el archivo en modo de escritura
@@ -38,5 +38,5 @@ int main() {
     const string nombreArchivo = "personas.txt";
     // Guardar los datos de las personas en el archivo
     guardarPersonas(nombreArchivo);
-    return 0;  // Salir con éxito
+    return 0;  // Salir con Ã©xito
 }
