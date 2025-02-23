bdefb2c6bfe8811e4c098f2ee78e1ddf94f822f9
Santiago Agostini <141504608+SantiCZ@users.noreply.github.com>
2025-02-23 12:59:19 -0300

    Update reloj_mundial.js

--- reloj mundial/reloj_mundial.js
+++ reloj mundial/reloj_mundial.js
@@ -1,8 +1,8 @@
 function updateTime() {
-    const nycTime = new Date().toLocaleString("en-US", { timeZone: "America/New_York" });
-    const londonTime = new Date().toLocaleString("en-GB", { timeZone: "Europe/London" });
-    const tokyoTime = new Date().toLocaleString("ja-JP", { timeZone: "Asia/Tokyo" });
-  
+    const nycTime = new Date().toLocaleString("en-US", { timeZone: "America/New_York" });//hora en nueva york
+    const londonTime = new Date().toLocaleString("en-GB", { timeZone: "Europe/London" });//hora en londres
+    const tokyoTime = new Date().toLocaleString("ja-JP", { timeZone: "Asia/Tokyo" });//hora en tokyo
+  //se puede poner cualquier ciudad que se quiera, pero se necesita modificar el HTML  y el CSS tambien
     document.getElementById("time-nyc").textContent = new Date(nycTime).toLocaleTimeString();
     document.getElementById("time-london").textContent = new Date(londonTime).toLocaleTimeString();
     document.getElementById("time-tokyo").textContent = new Date(tokyoTime).toLocaleTimeString();
